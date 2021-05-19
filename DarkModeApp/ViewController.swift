7361e30deba3dcc4f239a66737f66ae8982cd0b2
Bilgehan KAYA <bilgehankaya79@gmail.com>
2021-05-20 01:52:42 +0300

    App Finished

--- DarkModeApp/ViewController.swift
+++ DarkModeApp/ViewController.swift
@@ -1,19 +1,42 @@
-//
-//  ViewController.swift
-//  DarkModeApp
-//
-//  Created by Bilgehan KAYA on 19.05.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Uygulamanın tamamının modunu değiştirmek için aşağıdaki satırı info.plist e ekliyoruz
+        // User Interface Style -> (String) -> Light
+        //Üstteki seçenek çalışmadı
+        
+        
+        //eğer interface stilini sabitlemek istiyorsak aşağıdaki kodu kullanıyoruz
+        overrideUserInterfaceStyle = .light
+        
+        
+        //Eğer kullanıcının seçtiği mode a göre işlem yapacaksak aşağıdaki kodu kullanıyoruz
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.green
+        }
+        else {
+            changeButton.tintColor = UIColor.red
+        }
     }
-
-
+    
+    //bu event traitlerden biri değişince çalışıyor
+    //örneğin dark mode ile light mode geçişlerinde çalışıyor
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        //kullanıcı arayüzü hangi stilde kullanıyor
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.green
+        }
+        else {
+            changeButton.tintColor = UIColor.red
+        }
+    }
 }
 
